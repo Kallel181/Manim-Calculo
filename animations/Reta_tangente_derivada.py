f26f27c0bcdd0fa2fce0aaf8af01f662ed0009e1
Kallel181 <86331656+Kallel181@users.noreply.github.com>
2024-04-18 10:31:45 -0300

    Comments 1 -Reta_tagente

--- animations/Reta_tangente_derivada.py
+++ animations/Reta_tangente_derivada.py
@@ -3,7 +3,7 @@ from manim_slides import Slide
 
 class tangente(Slide):
     def construct(self):
-        #Definindo configurações do eixos
+        #Objeto Manim que armazena os eixos cartesianos
         axes = (
             Axes(
                 x_range = [0,10,1],
@@ -16,18 +16,27 @@ class tangente(Slide):
             .set_color(WHITE)
         )
         
+        #Adicionando texto para indicar os eixos
         axes_labels = axes.get_axis_labels(x_label="x",y_label="y")
         
+        #Função para desenharmos o grafico
         def f(x):
             return 0.1 * (x - 2) * (x - 5) * (x - 7) + 7
 
+        #Objeto Manim que armazena o grafico da função previamente definida
         graph = axes.plot(
             f, x_range=[0,10], color=BLUE
         )
 
+        #Value Trackers armazenam os valores de x e dx, e podem ser usados para animar a reta secante 
+        #posteriormente.
+        #Os valores iniciais podem ser alterados conforme necessidade.
         x = ValueTracker(7)
         dx = ValueTracker(2)
 
+        #Objeto Manim que armazena a reta secante ao grafico conforme x e dx.
+        #O objeto precisa estar dentro da função always_redraw uma vez que os calores de x e dx irão ser
+        #alterados posteriomente.
         secant = always_redraw(
             lambda: axes.get_secant_slope_group(
                 x = x.get_value(),
@@ -41,12 +50,18 @@ class tangente(Slide):
             )
         )
 
+        #Objeto Manim que armazena o ponto no grafico correspondente ao valor de x.
+        #O objeto precisa estar dentro da função always_redraw uma vez que os calores de x ira ser
+        #alterado posteriomente.
         dot1 = always_redraw(
             lambda: Dot()
             .scale(0.7)
             .move_to(axes.c2p(x.get_value(), f(x.get_value())))
         )
         
+        #Objeto Manim que armazena o ponto no grafico correspondente ao valor de x + dx.
+        #O objeto precisa estar dentro da função always_redraw uma vez que os calores de x e dx irão ser
+        #alterados posteriomente.
         dot2 = always_redraw(
             lambda: Dot()
             .scale(0.7)
@@ -56,6 +71,8 @@ class tangente(Slide):
             ))
         )
 
+        #Funções de animação
+        #Agora como todos os objetos já foram definidos, podemos continuar com a animação dos mesmos.
         self.add(axes,axes_labels,graph)
         self.wait(0.1)
         self.next_slide() 
@@ -63,6 +80,8 @@ class tangente(Slide):
         self.play(Create(VGroup(dot1,dot2,secant)))
         self.next_slide()
 
+        #Uma vez que os objetos que dependem do valor de x e dx foram colocados dentro das funções 
+        #always_redraw eles serão alterados de acordo conforme os valores de x e dx forem alterados.
         self.play(dx.animate.set_value(0.001),run_time=8)
         self.next_slide()
 
