47e7a08dca0cde820a8d6e0cdf439e3c400ba2c1
Kallel181 <86331656+Kallel181@users.noreply.github.com>
2024-04-21 11:16:24 -0300

    Max Mim derivative graph

--- animations/Maximos_e_minimos.py
+++ animations/Maximos_e_minimos.py
@@ -8,13 +8,17 @@ class maximo_minimo(Slide):
         WM.move_to([6,-3.5,0])
         self.add(WM)
         
+        #Objeto Manim que armazena o texto Latex da função as ser desenhada
+        function_tex = MathTex(r'f(x)=\frac{x^3}{10}-\frac{12x^2}{10}+\frac{45x}{10}-63')
         
-        function = MathTex(r'f(x)=\frac{x^3}{10}-\frac{12x^2}{10}+\frac{45x}{10}-63')
-        
-        derivative = MathTex('f\'(x)=\\frac{3x^2}{10}-\\frac{24x}{10}+\\frac{45}{10}')
+        #Objeto Manim que armazena o texto Latex da derivada da função as ser desenhada
+        derivative_tex = MathTex('f\'(x)=\\frac{3x^2}{10}-\\frac{24x}{10}+\\frac{45}{10}')
         #raizes: 5 e 3 
 
-        axes = (
+        #Objeto Manim que armazena os eixos cartesianos
+        #Com alterações na função a ser mostrada é importante mudar o valores do graficos para que
+        #a função seja melhor desenhada.
+        axes1 = (
             Axes(
                 x_range = [0,10,1],
                 x_length = 9,
@@ -26,16 +30,17 @@ class maximo_minimo(Slide):
         )
         
         #Adicionando texto para indicar os eixos
-        axes_labels = axes.get_axis_labels(x_label="x",y_label="y")
+        axes1_labels = axes1.get_axis_labels(x_label="x",y_label="y")
         
         #Função para desenharmos o grafico
         def f(x):
             return 0.1 * (x - 2) * (x - 5) * (x - 7) + 7
         
+        #Função para desenharmos o grafico da função derivada
         def derivative(x):
             return 0.1 * ((3*x**2)-(24*x)+45)
         
-        graph = axes.plot(
+        graph1 = axes1.plot(
             f, x_range=[0,10], color=BLUE
         )
 
@@ -43,11 +48,104 @@ class maximo_minimo(Slide):
         dx = 0.001
 
         tangent = always_redraw(
-            lambda: axes.get_secant_slope_group(
+            lambda: axes1.get_secant_slope_group(
                 x = x.get_value(),
-                graph = graph,
+                graph = graph1,
                 dx = dx,
                 secant_line_color = GREEN,
-                secant_line_length = 8,
+                secant_line_length = 4,
             )
         )
+
+        #Objeto Manim que armazena o ponto no grafico correspondente ao valor de p.
+        #O objeto precisa estar dentro da função always_redraw uma vez que os valores de p ira ser
+        #alterado posteriomente.
+        dot1 = always_redraw(
+            lambda: Dot()
+            .scale(0.7)
+            .move_to(axes1.c2p(x.get_value(),f(x.get_value())))
+        )
+        
+        #Movendo o ponto para a camada mais acima, para que a reta tangente e o grafico não sejam desenhados
+        #acima do ponto
+        dot1.z_index = 1
+
+        left_elements = VGroup(tangent,graph1,axes1,axes1_labels,function_tex)
+
+        axes2 = (
+            Axes(
+                x_range = [0,10,1],
+                x_length = 9,
+                y_range = [-1,5,1],
+                y_length = 6,
+                axis_config = {"include_numbers": True, "include_tip":False},
+            )
+            .set_color(WHITE)
+        )
+
+        axes2_labels = axes2.get_axis_labels(x_label="x",y_label="y")
+
+        graph2 = axes2.plot(
+            derivative, x_range=[0,7.5], color=BLUE
+        )
+
+        
+        derivative_tex.shift(UP*3)
+        right_elements = VGroup(derivative_tex,axes2,axes2_labels,graph2)
+        right_elements.scale(0.6)
+        right_elements.shift(RIGHT*4)
+        right_elements.shift(DOWN*0.5)
+
+
+        self.play(Write(function_tex))
+        self.wait(0.1)
+        self.pause()
+
+        function_tex_target = function_tex.generate_target()
+        function_tex_target.shift(UP*3)
+        self.play(MoveToTarget(function_tex))
+        self.wait(0.1)
+        self.pause()
+
+        self.play(Write(VGroup(axes1,graph1,axes1_labels,tangent,dot1)))
+        self.wait(0.1)
+        self.pause()
+
+        left_elements_target = left_elements.generate_target()
+        left_elements_target.scale(0.6)
+        left_elements_target.shift(LEFT*3)
+        self.play(MoveToTarget(left_elements))
+        self.wait(0.1)
+        self.pause()
+
+
+        self.play(Write(derivative_tex))
+        self.wait(0.1)
+        self.pause()
+
+        self.play(Write(VGroup(axes2,axes2_labels,graph2)))
+        self.wait(0.1)
+        self.pause()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        self.play(
+            *[FadeOut(mob)for mob in self.mobjects]
+        )
+        self.wait()
